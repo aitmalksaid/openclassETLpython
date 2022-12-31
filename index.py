2129c37dda617a5f7f56bd495575dbd209ea6364
said aitmalk <aitmalksaid@gmail.com>
2022-12-31 17:49:35 +0100

    parser tt les titres de la page et les associes a une variable nommé titres avec soap

--- index.py
+++ index.py
@@ -4,3 +4,6 @@ url = "https://www.gov.uk/search/news-and-communications"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.title)
+class_name = "gem-c-document-list__item-title"
+titres = soup.find_all("a", class_=class_name)
+print(titres)
