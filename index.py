a7e96e548856d8808516a7752bcb3727a9d7336a
said aitmalk <aitmalksaid@gmail.com>
2022-12-31 17:54:30 +0100

    extraire le a description de la page

--- index.py
+++ index.py
@@ -7,3 +7,5 @@ print(soup.title)
 class_name = "gem-c-document-list__item-title"
 titres = soup.find_all("a", class_=class_name)
 print(titres)
+descriptions = soup.find_all("p", class_="gem-c-document-list__item-description")
+print(descriptions)
